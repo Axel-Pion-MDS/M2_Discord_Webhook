dc6e03b6c971ef3ed95fa106702b6194bcdf5f7b
Axel Pion <pionaxel@gmail.com>
2024-02-15 15:43:22 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,47 @@ setup(
     author='Maengdok',
     author_email='maengdok@outlook.com',
     description='A Python package for easily sending messages through Discord webhooks',
-    long_description='This package provides a simple interface for sending messages through Discord webhooks. It includes a Webhook class with methods to define the content, mentions, and easily send messages with embedded content.',
+    long_description="""
+    # discord_webhook_easy_message
+
+    A Python package for easily sending messages through Discord webhooks.
+
+    ## Installation
+
+    Install the package using pip:
+
+    ```bash
+    pip install discord_webhook_easy_message
+    ```
+
+    ## Usage
+
+    ```python
+    from discord_webhook_easy_message import Webhook
+
+    # Create an instance of the Webhook class
+    webhook_instance = Webhook()
+
+    # Define the webhook URL
+    webhook_instance.define_webhook_url("your_discord_webhook_url")
+
+    # Define mentions (optional)
+    webhook_instance.define_mentions(["userId1", "userId2"])
+
+    # Define content (optional)
+    webhook_instance.define_content("Hello @user1, check this out!")
+
+    # Send a message
+    webhook_instance.send(
+        title="New Release",
+        description="Version 1.0 is now available!",
+        author="Your Bot",
+        color="00FF00",
+        url="https://github.com/yourusername/yourrepository/releases/tag/v1.0"
+    )
+    ```
+    """,
+    long_description_content_type='text/markdown',
     url='https://github.com/Axel-Pion-MDS/M2_Discord_Webhook',
     classifiers=[
         'Development Status :: 3 - Alpha',
